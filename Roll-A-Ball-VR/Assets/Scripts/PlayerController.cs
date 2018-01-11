be00c93ed7bae0dff5ae7830e34f7d4ab22ebf28
James Wang <james.k.wang.18@dartmouth.edu>
2018-01-11 00:02:22 -0500

    adding AndroidAppVR w/ incorrect controller functionality

---
+++ Roll-A-Ball-VR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		if (OVRInput.IsControllerConnected (OVRInput.Controller.RTrackedRemote)) 
+		{
+			if(moveHorizontal == 0 && moveVertical == 0)
+			{
+				if (OVRInput.GetDown (OVRInput.Button.Any))
+				{
+					moveHorizontal = 1;
+					moveVertical = 1;
+				}
+			}
+		}
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		} 
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "You Win";
+		}
+	}
+}
